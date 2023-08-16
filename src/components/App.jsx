5396a9aef2e6ce50dba0cab9a5fced9998ab7504
ribacot <remontdiskov1979@gmail.com>
2023-08-16 23:29:05 +0300

    1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,11 @@
+import { useEffect } from 'react';
+import movieApi from './service/serviseBaseApi'
 export const App = () => {
+
+  useEffect(()=>{movieApi()},[])
   return (
     <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
     >
-      React homework template by Andreii))
     </div>
   );
 };
