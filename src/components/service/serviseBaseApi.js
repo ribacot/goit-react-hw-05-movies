725f45b56dcda91020430796fb9a518497b6585a
ribacot <remontdiskov1979@gmail.com>
2023-08-21 14:30:06 +0300

    Update serviseBaseApi.js

--- src/components/service/serviseBaseApi.js
+++ src/components/service/serviseBaseApi.js
@@ -2,7 +2,9 @@ import axios from 'axios';
 
 export  const movieApi = axios.create({
  baseURL:'https://api.themoviedb.org/3/',
+ method: 'get',
   headers: {
+    'accept': 'application/json',
   'Authorization':'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhYzY0NTBkYTE3OWZlNDIxY2Y5NzQyYWU2OWEzM2RlNCIsInN1YiI6IjY0ZGI1NWJmMDAxYmJkMDBlMzVkMzc1YyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.QMhBhXm6B0N1kwqvUOCBJyHASuDqGrhcINSUolbmq0w',
 },
 })
