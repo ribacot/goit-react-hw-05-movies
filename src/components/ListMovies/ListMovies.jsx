e558f909397da4d8852d1138ef6f9b725d718387
ribacot <remontdiskov1979@gmail.com>
2023-08-24 16:25:48 +0300

    Update ListMovies.jsx

--- src/components/ListMovies/ListMovies.jsx
+++ src/components/ListMovies/ListMovies.jsx
@@ -1,17 +1,17 @@
-import { Link } from "react-router-dom";
-import TradingItem from "Pages/TrandingMovies/TradingItem/TradingItem";
+// import { Link } from "react-router-dom";
+// import TradingItem from "Pages/TrandingMovies/TradingItem/TradingItem";
 
 
-export default function ListMovies({movies}) {
-  return (
-    <ul className="list_movie">
-      {movies.map(({ id, poster_path }) => (
-        <li key={id} className='link'>
-          <Link to={`selectedmovie/${id}`} state={{ from: location }}>
-            <TradingItem img={poster_path} />
-          </Link>
-        </li>
-      ))}
-    </ul>
-  );
-}
+// export default function ListMovies({movies}) {
+//   return (
+//     <ul className="list_movie">
+//       {movies.map(({ id, poster_path }) => (
+//         <li key={id} className='link'>
+//           <Link to={`selectedmovie/${id}`} state={{ from: location }}>
+//             <TradingItem img={poster_path} />
+//           </Link>
+//         </li>
+//       ))}
+//     </ul>
+//   );
+// }
